3220da6b93c8a4516a6c6c60ba6c607f8ec374db
Michael Scheffenacker <msc@formiculare.org>
2022-09-29 08:39:13 +0200

    test: added test for cocktail duplicates

--- test/utils.test.js
+++ test/utils.test.js
@@ -45,4 +45,55 @@ describe('mapIngredientToCocktail', () => {
         let result = utils.mapIngredientToCocktail(input);
         expect(result).toStrictEqual(expected);
     });
+
+    test('input double entry', () => {
+        let input = [
+            {
+                "id": 1,
+                "key": "12460",
+                "title": "Vodka And Tonic",
+                "ingredients": ["Vodka", "Tonic water"]
+            },
+            {
+                "id": 2,
+                "key": "16041",
+                "title": "Mudslinger",
+                "ingredients": ["Southern Comfort", "Orange juice", "Pepsi Cola"]
+            },
+            {
+                "id": 3,
+                "key": "12674",
+                "title": "Fruit Shake",
+                "ingredients": ["Yoghurt", "Banana"]
+            },
+            {
+                "id": 4,
+                "key": "16041",
+                "title": "Mudslinger",
+                "ingredients": ["Southern Comfort", "Orange juice", "Pepsi Cola"]
+            }
+        ]
+        let expected = {
+            "Southern Comfort": [{
+                "id": 2,
+                "ingredients": ["Southern Comfort", "Orange juice", "Pepsi Cola"],
+                "key": "16041",
+                "title": "Mudslinger"
+            }],
+            "Vodka": [{
+                "id": 1,
+                "ingredients": ["Vodka", "Tonic water"],
+                "key": "12460",
+                "title": "Vodka And Tonic"
+            }],
+            "Yoghurt": [{
+                "id": 3,
+                "ingredients": ["Yoghurt", "Banana"],
+                "key": "12674",
+                "title": "Fruit Shake"
+            }]
+        }
+        let result = utils.mapIngredientToCocktail(input);
+        expect(result).toStrictEqual(expected);
+    });
 });
